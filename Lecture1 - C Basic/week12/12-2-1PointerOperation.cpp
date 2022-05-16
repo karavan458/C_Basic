0b6991887ebbfb50342981905b1c80468133dc1a
karavan458 <karavan458@gmail.com>
2022-05-16 20:13:14 +0900

    add week12

---
+++ Lecture1 - C Basic/week12/12-2-1PointerOperation.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int num1 = 100, num2 = 200;
+    int * pnum;
+    
+    pnum = &num1;
+    (*pnum) += 30;
+
+    pnum = &num2;
+    (*pnum) -= 30;
+
+    printf("num1 = %d, num2 = %d", num1, num2);
+    return 0;
+}
