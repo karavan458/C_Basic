df1756c4672e5da46e279bf0a2d10ce19ccf3394
karavan458 <karavan458@gmail.com>
2022-05-24 10:13:44 +0900

    add part of week17

---
+++ Lecture1 - C Basic/week17/17-1-3PointerSwapSuccess.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void SwapIntPtr(int ** p1, int ** p2) {
+    int * temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    int * ptr1 = &num1;
+    int * ptr2 = &num2;
+
+    printf("%d, %d \n", *ptr1, *ptr2);
+
+    SwapIntPtr(&ptr1, &ptr2);
+    printf("%d, %d \n", *ptr1, *ptr2);
+
+    return 0;
+}
+
